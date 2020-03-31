400481fd71d8ccd7584baec57726d725d60a049b
Archies <archisbhoir2016@gmail.com>
2020-04-01 00:33:54 +0530

    table and form routes with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import { Switch, Route} from "react-router-dom";
+import AppNavbar from './components/AppNavbar';
+import Form from './components/Form'
+import Table from './components/Table'
+import Home from './components/Home'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <AppNavbar/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/table" component={Table}/>
+          <Route path="/form" component={Form}/>
+        </Switch>
+        {this.props.children}
+      </div>
+    )
+  }
 }
 
-export default App;
